4a426507e67b480fd8adcf20a0ff8b2be0213cc3
Muhammad Bashir <bashirkarimi5@gmail.com>
2024-09-27 14:50:30 +0200

    feat: streamline and move data fetching inside component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,15 @@
 import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import { fetchLatestInvoices, fetchCardData } from "../../lib/data";
+import { fetchCardData } from "../../lib/data";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from "@/app/ui/skeletons";
 import { lusitana } from "@/app/ui/fonts";
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
   const { numberOfInvoices, totalPaidInvoices, totalPendingInvoices, numberOfCustomers } = await fetchCardData();
   return (
     <main>
@@ -28,7 +30,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+        <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
